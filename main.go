381154b97b318a3731dcd02d439838c824a06d68
Raghav Dogra <rdogra@cs.stonybrook.edu>
2018-12-12 20:21:30 -0500

    SIM working which matches GPU

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 // #cgo pkg-config: opencv
-// #cgo LDFLAGS: -Lcaffe/lib 
 // #cgo CXXFLAGS: -std=c++11 -Icaffe/include -I/usr/include/opencv -I.. -O2 -fomit-frame-pointer -Wall
 // #include <stdlib.h>
+// #include <zmq.h>
 // #include "classification.hpp"
+// #cgo windows LDFLAGS: -L/usr/local/lib -lzmq
 import "C"
 
 import (
+//	"bytes"
+	"bytes"
+	"encoding/binary"
 	"time"
         "io"
-        "io/ioutil"
+       // "io/ioutil"
 	"log"
 	"os"
 	"net/http"
 	"strconv"
+//	"image"
+	"image/jpeg"
+	zmq "github.com/pebbe/zmq4"
 )
 var n uint32
 var cstr *C.char
@@ -30,6 +37,9 @@ var w2 [10]http.ResponseWriter
 var mux map[string]func(http.ResponseWriter, *http.Request)
 var responseReady bool
 
+type pixel struct{
+	r,g,b uint32
+}
 
 type myHandler struct{}
 
@@ -44,7 +54,8 @@ func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 type job struct {
 	ch chan string
-	buf []byte 
+	buf []byte  
+	batchsize int
 }
 var c chan job	//c is the channel from which mainloop receives forever.i
 var gpu_channel chan job
@@ -63,7 +74,7 @@ func mainloop() {
 	i:=0
 	for  {
 		tick := time.Tick(time.Duration(t) * time.Millisecond)
-		jobs:= [10]job{}
+		jobs:= [8]job{}
                 for i=0;i<n;i++{
 			select {
 			case	jobs[i] = <-c:
@@ -81,30 +92,98 @@ func mainloop() {
 		}
 		if i!=0 {
 		go processbatch(jobs,i)
+//		go argusBackend(jobs,i)
 		}
 	}
 }
-func stage2(j job){
-	time.Sleep(9*time.Millisecond)
+func stage2(j job, count int){
+	log.Println(time.Duration(count)*time.Millisecond)
+	time.Sleep(time.Duration(count)*time.Millisecond)
 	j.ch <-"Rat"
 }
 func dummygpu() {
-	gpu_channel = make(chan job, 4)
+	gpu_channel = make(chan job, 1)
 	job_num := 0
 	for true {
 
 			currjob := <-gpu_channel
 			job_num = job_num + 1
-			log.Print("gpu request# ",job_num)
-			time.Sleep(3 * time.Millisecond)
-			go stage2(currjob)
+			var latency int
+			var throughput int
+			log.Print("gpu request# ",job_num, "bSize: ",currjob.batchsize)
+			switch  currjob.batchsize {
+			case 1 :
+				throughput = 18
+				latency = 27
+			case 2 :
+				throughput = 26
+				latency = 36
+			case 3 :
+				throughput = 32
+				latency = 44
+			case 4 :
+				throughput = 40
+				latency = 53
+			case 5 :
+				throughput = 48
+				latency = 62
+			case 6 :
+				throughput = 55
+				latency = 70
+			case 7 :
+				throughput = 64
+				latency = 81
+			case 8 :
+				throughput = 73
+				latency = 91
+			}
+			log.Println(time.Duration(throughput)*time.Millisecond)
+			time.Sleep((time.Duration(throughput) * time.Millisecond))
+			go stage2(currjob, latency-throughput)
 	}
 }
-func processbatch(jobs [10]job, count int ) {
+
+func argusBackend(jobs [8]job, count int) {
+	context, _ := zmq.NewContext()
+	//defer context.Close()
+	zsock, _ := context.NewSocket(zmq.DEALER)
+	defer zsock.Close()
+	zsock.Connect("tcp://localhost:10090")
+	var s []string
+	s = make([]string, count, count)
+	for i:=0;i<count;i++ {
+		//zsock.Send(string(jobs[i].buf),Flag(C.ZMQ_SNDMORE))
+		s[i] = string(jobs[i].buf)
+	}
+	zsock.SendMessage(s)
+	msg, _ := zsock.RecvMessage(0)
+//	log.Println("Received from Argus Backend")
+//	log.Println(len(msg))
+	for i:=0;i<count;i++ {
+	//	zsock.Send(string(jobs[i].buf),Flag(C.ZMQ_SNDMORE))
+	//	log.Println(len(msg[i]))
+	//	log.Printf("type is %T",msg[i])
+	var fres [1000]float32
+	arr := [] byte(msg[i])
+//	for i:=0 ; i < 4000 ;i++ {
+//		log.Print(arr[i])
+//	}
+	buffferReceived := bytes.NewReader(arr)
+	err := binary.Read(buffferReceived , binary.LittleEndian, &fres)
+	if err != nil {
+		log.Println("binary.Read failed:", err)
+	}
+	log.Println(fres[0])
+		jobs[i].ch <- string(len(fres))
+		//log.Println(msg[i])
+	}
+}
+
+func processbatch(jobs [8]job, count int ) {
 	buf1 := jobs[0].buf
 	res_chan := make (chan string)
 	log.Print("sending to gpu and count is ", count)
-	gpu_channel <- job{res_chan,buf1}
+	gpu_channel <- job{res_chan,buf1,count}
 	log.Print("waiting to recieve from res_chan")
 	cstr := <-res_chan
 	log.Print("recieved from res_chan")
@@ -115,20 +194,57 @@ func processbatch(jobs [10]job, count int ) {
 }
 
 func modclass1(w http.ResponseWriter, r *http.Request) {
-        buffer, err := ioutil.ReadAll(r.Body)
+/*        buffer, err := ioutil.ReadAll(r.Body)
         if err != nil {
                 http.Error(w, err.Error(), http.StatusBadRequest)
         return
         }
+	log.Print(len(buffer))*/
+	im, err := jpeg.Decode(r.Body)
+        if err != nil {
+		log.Print("Decode Failed, returning")
+               http.Error(w, err.Error(), http.StatusBadRequest)
+      		return
+        }
+//	bounds := im.Bounds()
+	
+//	log.Print(bounds)
+/*
+//	log.Print(len(string(im)))
+//	m0 := image.NewRGBA(image.Rect(0, 0, 224, 224))
+	imag := im.SubImage(image.Rect(0, 0, 224, 224)).(*image.RGBA)
+	var buf bytes.Buffer
+	err = jpeg.Encode(&buf, im, nil)
+	if err != nil {
+		log.Print("Encode Failed, returning")
+               http.Error(w, err.Error(), http.StatusBadRequest)
+		return 
+	}
+	log.Print(len(buf.Bytes()))
+	//newImage := resize.Resize(160, 0, original_image, resize.Lanczos3)
+	//log.Print(buff)*/
+	pixels := make([]pixel, 224*224)
+	pbuffer := make([]byte, 224*224*3*4)
+	for i:=0; i < 224*224; i++ {
+		x := i%224;
+		y := i/224;
+		r,g,b,_ := im.At(x,y).RGBA()
+		pixels[i].r = r
+		pixels[i].g = g
+		pixels[i].b = b	
+		*(&pbuffer[i*12 + 4]) = byte(g)
+		*(&pbuffer[i*12]) = byte(r)
+		*(&pbuffer[i*12 + 8]) = byte(b)
+	}
 	ch := make(chan string) //creates it's own channel from which it will expect to receive value back from GPU!
-	c<-job{ch,buffer} //sends the job structure to c
+	c<-job{ch,pbuffer,0} //sends the job structure to c
 	gostr := <-ch	//waits to receive from channel which it passed on that it'll receive from that!
 	io.WriteString(w, gostr )
 }
 
 func main() {
 	srv := http.Server{
-		Addr:    ":8001",
+		Addr:    ":8002",
 		ReadTimeout: 100 * time.Second,
 		WriteTimeout: 100 * time.Second,
 		Handler: &myHandler{},
@@ -143,7 +259,7 @@ func main() {
 	go dummygpu()//concurrent dummy gpu running
 	defer C.classifier_destroy(ctx)
 	log.Println("Adding REST endpoint /api/classify")
-	log.Println("Starting server listening on :8000")
+	log.Println("Starting server listening on :8002 with SIM backend")
 	log.Fatal(srv.ListenAndServe())
  }
 
